2f108f77bc5ed784d55a705218c1646c2ec71017
lwy <975139902@qq.com>
2018-12-09 20:00:19 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// 导入Vue包
+import Vue from 'vue'
+import app from './app.vue'
+
+// 导入mui的样式
+import './lib/mui/css/mui.min.css'
+//按需引入mint-ui提供的组件
+import {Header} from 'mint-ui'
+import 'mint-ui/lib/style.css'
+// 引入后需要声明为全局组件，可以让别的组件来使用
+Vue.component(Header.name,Header)
+
+// 构造Vue实例
+var vm=new Vue({
+    el:'#app',
+    render:function(createElements){
+        return createElements(app);
+    }
+}) 
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+module.exports = {
+    mode: "development",
+    entry: path.join(__dirname, 'src', 'main.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        alias: {
+            "vue$": "vue/dist/vue.js"
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }, {
+            test: /\.less$/,
+            use: ['style-loader','css-loader', 'less-loader']
+        }, {
+            test: /\.scss/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }, {
+            test: /\.vue$/,
+            use: ['vue-loader']
+        }, {
+            test: /\.(jpg|png|gif|bmp|jpeg)$/,
+            use: 'url-loader'
+        }, {
+            test: /\.(otf|eot|svg|ttf|woff|woff2)$/,
+            use: 'url-loader'
+        }]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
